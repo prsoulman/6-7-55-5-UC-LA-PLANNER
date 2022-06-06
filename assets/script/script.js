12b3072961083c435efe345aff27aa7d317c9807
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-06-06 09:58:41 -0500

    replaced jquery ui, added color conditional statements, added local storage saving

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,42 +1,58 @@
-var today = new Date();
-var hourNow = today.getHours();
+var today = moment().format('dddd, MMMM Do, YYYY');
+//var hourNow = today.getHours();
 var greeting;
 var header = $(".current");   
   // tweak needed for different time zones
-
-
+var currentHour=moment().hours()
+console.log(currentHour);
 // THEN the current day is displayed at the top of the calendar
 function displayDate () {
     //header.appendchild(today);
-    header.textcontent(today); 
+  header.text(today);
+}
+for (var i =9; i<18; i++) {
+  var colorKey='';
+  if (currentHour>i){
+    colorKey='past';
+  } 
+  else if (currentHour===i) {
+    colorKey='present';
+  }
+  else {
+    colorKey='future';
+  }
+  var hourDisplay='';
+  if (i<12) {
+    hourDisplay=i+' AM';
+  }
+  else if (i===12) {
+    hourDisplay=i+ ' PM';
+  }
+  else {
+    hourDisplay=i-12+ ' PM';
+  }
+  var rowdiv=$('<div>').addClass('row time-block').attr('id',i);
+var hourDiv=$('<div>').addClass('col-2 hour').text(hourDisplay);
+var textDiv=$('<textarea>').addClass('col-8 description '+ colorKey).val(localStorage.getItem(i));
+var saveBtn=$('<button>').addClass('col-2 saveBtn').text('save').val(i);
+  $('.container').append(rowdiv.append(hourDiv, textDiv, saveBtn));
 }
 
-
 //time blocks for standard business hours. get from jquery
-$( function displayBlocks () {
-    $( "#selectable" ).selectable({
-      stop: function() {
-        var result = $( "#select-result" ).empty();
-        $( ".ui-selected", this ).each(function() {
-          var index = $( "#selectable li" ).index( this );
-          result.append( " #" + ( index + 1 ) );
-        });
-      }
-    });
-
-});
-
-
 
 
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 
 // WHEN I click into a time block THEN I can enter an event
-
+$('.saveBtn').on('click', function(){
+  var hourKey=this.value;
+  var activity=$(this).siblings('.description').val();
+  localStorage.setItem(hourKey, activity);
+})
 // text for that event is saved in local storage WHEN I refresh the page THEN the saved events persist
 
 
 
-displayBlocks ()
+
 displayDate ()
